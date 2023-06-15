b316b6ce9434b021b9ab58d0294f6ffd984e0882
Dhruvsaluja0411 <dhruvsaluja4@gmail.com>
2023-06-15 19:48:45 +0530

    frontend

---
+++ AudioBridge/script.js
@@ -0,0 +1,163 @@
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
+
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
+}
+
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+        // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+      // let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
+
+
+
+// const wrapper1 = document.querySelector(".wrapper1"),
+// selectBtn1 = wrapper1.querySelector(".select-btn1"),
+// searchInp1 = wrapper1.querySelector(".input"),
+// options1 = wrapper1.querySelector(".options1");
+// let countries1 = ["English", "Hindi", "Chinese", "Spanish", "Tamil", "Malyalam", "Telugu",
+//                  "French", "Gujarati", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
+//                  "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
+//                  "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
+//                  "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
+//                  "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+// function addCountry(selectedCountry) {
+//     options1.innerHTML = "";
+//     countries1.forEach(country => {
+//         let isSelected = country == selectedCountry ? "selected" : "";
+//         let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
+//         options1.insertAdjacentHTML("beforeend", li);
+//     });
+// }
+// addCountry();
+// function updateName(selectedLi) {
+//     searchInp.value = "";
+//     addCountry(selectedLi.innerText);
+//     wrapper1.classList.remove("active");
+//     selectBtn1.firstElementChild.innerText = selectedLi.innerText;
+// }
+// searchInp1.addEventListener("keyup", () => {
+//     console.log("fun");
+//     let arr = [];
+//     let searchWord = searchInp1.value.toLowerCase();
+//     arr = countries1.filter(data => {
+//         return data.toLowerCase().startsWith(searchWord);
+//     }).map(data => {
+//         let isSelected = data == selectBtn1.firstElementChild.innerText ? "selected" : "";
+//         return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+//     }).join("");
+//     options1.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
+// });
+// selectBtn1.addEventListener("click", () => wrapper1.classList.toggle("active"));
+
+
+const wrapper = document.querySelector(".wrapper"),
+selectBtn = wrapper.querySelector(".select-btn"),
+searchInp = wrapper.querySelector("input"),
+options = wrapper.querySelector(".options");
+let countries = ["English", "Hindi", "Chinese", "Spanish", "Tamil", "Malyalam", "Telugu",
+                 "French", "Gujarati", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
+                 "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
+                 "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
+                 "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
+                 "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+function addCountry(selectedCountry) {
+    options.innerHTML = "";
+    countries.forEach(country => {
+        let isSelected = country == selectedCountry ? "selected" : "";
+        let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
+        options.insertAdjacentHTML("beforeend", li);
+    });
+}
+addCountry();
+function updateName(selectedLi) {
+    console.log("hi1");
+    searchInp.value = "";
+    addCountry(selectedLi.innerText);
+    wrapper.classList.remove("active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
+searchInp.addEventListener("keyup", () => {
+    console.log("hi");
+    let arr = [];
+    let searchWord = searchInp.value.toLowerCase();
+    arr = countries.filter(data => {
+        return data.toLowerCase().startsWith(searchWord);
+    }).map(data => {
+        let isSelected = data == selectBtn.firstElementChild.innerText ? "selected" : "";
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
+});
+selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
